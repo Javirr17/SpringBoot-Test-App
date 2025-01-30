fed8866830193902959bc6739367383e78955b12
Javier Romero Rodriguez <95828158+Javirr17@users.noreply.github.com>
2025-01-30 13:28:28 +0100

    first commit

---
+++ test/src/main/java/com/example/test/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.example.test.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<Object> hello() {
+
+        return new ResponseEntity<>("Hello: v0", HttpStatus.OK);
+
+    }
+}
